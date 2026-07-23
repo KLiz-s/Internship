66ee7a91f07d0839aa7770fed59f3145b0ce6b8b
Elizaveta Klyndyuk <eklyndyuk@gmail.com>
2026-07-23 15:17:12 +0300

    feat: add DTOs and MapStruct mappers

---
+++ user-service/src/main/java/com/klyndyuk/userservice/dto/request/CreateUserRequest.java
@@ -0,0 +1,32 @@
+package com.klyndyuk.userservice.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateUserRequest {
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+
+    @NotBlank
+    @Size(max = 100)
+    private String surname;
+
+    @Past
+    @NotNull
+    private LocalDate birthDate;
+
+    @NotBlank
+    @Email
+    @Size(max = 255)
+    private String email;
+
+}
