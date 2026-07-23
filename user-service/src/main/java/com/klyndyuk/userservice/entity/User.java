ef490ff5b82b6413091ca6c8ef55724f749d2d61
Elizaveta Klyndyuk <eklyndyuk@gmail.com>
2026-07-23 11:45:25 +0300

    feat: implement user and payment card domain model

---
+++ user-service/src/main/java/com/klyndyuk/userservice/entity/User.java
@@ -0,0 +1,39 @@
+package com.klyndyuk.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User extends BaseEntity {
+    @OneToMany(
+        mappedBy = "user",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true
+    )
+    private List<PaymentCard> paymentCards = new ArrayList<>();
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "active")
+    private boolean active;
+}
